9b88799f0fcb1c0b5d1ad1b1f4cf131759065814
Nguyễn Danh Khuê <khuendhe181796@fpt.edu.vn>
2025-08-15 11:37:40 +0700

    update
    update

--- src/java/model/Topic.java
+++ src/java/model/Topic.java
@@ -4,18 +4,57 @@
  */
 package model;
 
-import lombok.*;
-
-/**
- *
- * @author sondo
- */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Topic {
     private Long topic_id;
     private String name;
+    private String thumbnail_Url;
     private String description;
+
+    public Topic() {
+    }
+
+    public Topic(Long topic_id, String name, String thumbnail_Url, String description) {
+        this.topic_id = topic_id;
+        this.name = name;
+        this.thumbnail_Url = thumbnail_Url;
+        this.description = description;
+    }
+
+    public Long getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(Long topic_id) {
+        this.topic_id = topic_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail_Url() {
+        return thumbnail_Url;
+    }
+
+    public void setThumbnail_Url(String thumbnail_Url) {
+        this.thumbnail_Url = thumbnail_Url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" + "topic_id=" + topic_id + ", name=" + name + ", thumbnail_Url=" + thumbnail_Url + ", description=" + description + '}';
+    }
+    
 }
